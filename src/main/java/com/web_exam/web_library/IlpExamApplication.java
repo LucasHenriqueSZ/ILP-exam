1bebb5e700a6100fec739d573cb84cf15acdd6bd
lucas <lucash100souza@gmail.com>
2024-09-06 21:23:37 -0300

    first commit

---
+++ src/main/java/com/web_exam/web_library/IlpExamApplication.java
@@ -0,0 +1,13 @@
+package com.web_exam.web_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlpExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlpExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web_exam/web_library/IlpExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_exam.web_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlpExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
